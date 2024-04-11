02240fbbba5a4431afc62509fd1817259d3d0ea4
One Direction <78015604+zhang2657977442@users.noreply.github.com>
2024-04-11 10:45:06 +0800

    Update WXConfigConstant.java

--- src/main/java/com/example/wuyou/constant/WXConfigConstant.java
+++ src/main/java/com/example/wuyou/constant/WXConfigConstant.java
@@ -3,7 +3,7 @@ package com.example.wuyou.constant;
 public interface WXConfigConstant {
     String CODE_URL = "https://api.weixin.qq.com/sns/jscode2session?appid=";
 
-    String APP_ID = "你好APP_ID";
+    String APP_ID = "你的APP_ID";
 
     String SECRET = "你的密钥";
 }
